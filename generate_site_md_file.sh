5ffc50c0d0a8daa1c402a2bbf71c2f3e69c7a8b2
alex3w <uyghjjjjjj@hotmail.com>
2016-06-13 23:46:58 +0800

    update

--- generate_site_md_file.sh
+++ generate_site_md_file.sh
@@ -2,9 +2,6 @@
 # -----------------------------------------------------
 # 把目录下所有 README.md 内容转换为 page 格式内容 
 #
-# https://github.com/zhengxiaopeng/android-dev-bookmarks/blob/master/generate_site_md_file.sh
-# 
-# Rocko(rocko.xyz)
 # -----------------------------------------------------
 
 FILE_LIST=`ls -d */|sed 's|[/]||g'` 
